1c944161d81f827861eb7024d79b5a446bbb7d49
Joshua Bautista <bautistajosh013@gmail.com>
2021-04-05 00:18:02 -0400

    pos tagging

---
+++ main.py
@@ -0,0 +1,21 @@
+import nltk
+
+
+class SettingGenerator:
+    def __init__(self, text: str):
+        self.text = text
+
+    def find_sensory_pairs(self) -> list:
+        tokens = nltk.word_tokenize(self.text)
+        tokens_tagged = nltk.pos_tag(tokens, tagset='universal')
+        sensory_pairs = list()
+        for i in range(len(tokens_tagged)):
+            if tokens_tagged[i][1] == 'ADJ' and tokens_tagged[i + 1][1] == 'NOUN':
+                sensory_pairs.append(' '.join([tokens_tagged[j][0] for j in range(i, i + 2)]))
+        return sensory_pairs
+
+
+if __name__ == '__main__':
+    my_text = open('setting.txt', 'r').read()
+    sg = SettingGenerator(my_text)
+    print(sg.find_sensory_pairs())
